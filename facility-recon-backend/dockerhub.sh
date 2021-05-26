9d4480649feaa0e9248c01241be45da389cc48d8
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2021-05-26 15:42:55 -0400

    start refactor docker for new gofr

--- facility-recon-backend/dockerhub.sh
+++ facility-recon-backend/dockerhub.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # automate tagging with the short commit hash
-docker build --no-cache -t intrahealth/facility-registry:$(git rev-parse --short HEAD) .
-docker tag intrahealth/facility-registry:$(git rev-parse --short HEAD) intrahealth/facility-registry
-docker push intrahealth/facility-registry:$(git rev-parse --short HEAD)
-docker push intrahealth/facility-registry:latest
+docker build --no-cache -t intrahealth/gofr:$(git rev-parse --short HEAD) .
+docker tag intrahealth/gofr:$(git rev-parse --short HEAD) intrahealth/gofr
+docker push intrahealth/gofr:$(git rev-parse --short HEAD)
+docker push intrahealth/gofr:latest

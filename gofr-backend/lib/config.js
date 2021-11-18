e4953031c15e466ddf22f5b50d09bd0bd1a19c14
Ally Shaban <allyshaban5@gmail.com>
2021-11-18 18:40:37 +0300

    troubleshooting docker

--- gofr-backend/lib/config.js
+++ gofr-backend/lib/config.js
@@ -4,4 +4,6 @@ nconf.argv()
   .env({ separator: '__' })
   .file(`${__dirname}/../config/default.json`);
 
+console.log(JSON.stringify(process.env, 0, 2));
+console.log(nconf.get("redis:host"));
 module.exports = nconf;

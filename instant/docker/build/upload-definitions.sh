7e892dd62ec7c6af9bb346377bf19d0aeff0cd30
Ally Shaban <allyshaban5@gmail.com>
2021-11-23 09:37:03 +0300

    ensure that definitions arent uploaded twice

--- instant/docker/build/upload-definitions.sh
+++ instant/docker/build/upload-definitions.sh
@@ -9,7 +9,7 @@ do
     esac
 done
 
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://${host}:${port}/fhir/DEFAULT/ValueSet/location-status/$expand)
+status_code=$(dockerize -wait-retry-interval 5s -timeout 120s -wait http://${host}:${port}/fhir/DEFAULT/metadata curl --write-out %{http_code} --silent --output /dev/null http://${host}:${port}/fhir/DEFAULT/ValueSet/location-status/$expand)
 
 if [[ "$status_code" -ne 200 ]] ; then
   ./hapi-fhir-cli upload-definitions -v r4 -t http://${host}:${port}/fhir/DEFAULT

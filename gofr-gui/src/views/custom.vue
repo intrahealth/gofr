8c505d90e98838b7e11b2376f038a9f981029e62
Ally Shaban <allyshaban5@gmail.com>
2023-09-23 10:07:23 +0300

    changing the way of loading custom components

--- gofr-gui/src/views/custom.vue
+++ gofr-gui/src/views/custom.vue
@@ -1,13 +1,33 @@
 <template>
-  <component :is="currentComponent"></component>
+  <component 
+    :is="currentComponent" 
+    :queries="this.$route.query" 
+  />
 </template>
 <script>
 export default {
-props: ["path", "component"],
-data() {
-  return {
-      currentComponent: () => import(`../../site/${this.$route.params.path}/${this.$route.params.component}.vue`)
+  props: ["component"],
+  data() {
+    return {
+        currentComponent: ""
+    }
+  },
+  created() {
+    let componentPath = ""
+    if(this.$route.query.path) {
+      let actualPath = this.$route.query.path.split('/')
+      for(let path of actualPath) {
+        if(!path) {
+          continue
+        }
+        componentPath += "/" + path
+      }
+    }
+    componentPath += "/" + this.$route.params.component
+    if(!componentPath.endsWith(".vue")) {
+      componentPath += ".vue"
+    }
+    this.currentComponent = () => import(`@/site${componentPath}`)
   }
 }
-}
-</script>>
+</script>

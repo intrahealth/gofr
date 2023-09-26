5e8c1831ea71846ec1d02b2f6e4c8c66c35dfc77
Ally Shaban <allyshaban5@gmail.com>
2023-09-26 13:00:53 +0300

    rebuilding app

---
+++ gofr-backend/lib/gofr-backend-site/gui/js/chunk-2d0b2ea8.fe000e1f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2ea8"],{"25c2":function(e,n,l){"use strict";l.r(n);var t=function(){var e=this,n=e.$createElement,l=e._self._c||n;return l("div",[e._v(" Hello ")])},c=[],u=l("2877"),a={},o=Object(u["a"])(a,t,c,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0b2ea8.fe000e1f.js.map
---
+++ gofr-backend/lib/gofr-backend-site/gui/js/chunk-2d0cef8e.192ffe91.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cef8e"],{6273:function(e){e.exports=JSON.parse('{"name":"site","version":"1.0.0","description":"","main":"app.js","scripts":{"test":"echo \\"Error: no test specified\\" && exit 1"},"author":"Ally Shaban","license":"ISC"}')}}]);
+//# sourceMappingURL=chunk-2d0cef8e.192ffe91.js.map

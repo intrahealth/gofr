c5a79d6f964afcff56610a482eab821c685892ae
Ally Shaban <allyshaban5@gmail.com>
2023-12-20 14:13:36 +0300

    setting database credentials inside config file

--- gofr-backend/lib/modules/postgres.js
+++ gofr-backend/lib/modules/postgres.js
@@ -1,10 +1,11 @@
 const { Pool, escapeLiteral } = require("pg");
+const config = require('../config');
 
 const pool = new Pool({
-  database: "gofr",
-  user: "hapi",
-  password: "hapi",
-  port: 5432,
-  host: "localhost",
+  database: config.get('database:dbname'),
+  user: config.get('database:username'),
+  password: config.get('database:password'),
+  port: config.get('database:port'),
+  host: config.get('database:host')
 });
 module.exports = { pool, escapeLiteral };
